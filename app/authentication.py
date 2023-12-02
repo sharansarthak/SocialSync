071281b39ae08fd0a4f0cfcc732beaf0d07602ce
Sarthak <sharansarthak@gmail.com>
2023-12-01 19:45:31 -0700

    Adding env keys for config

--- app/authentication.py
+++ app/authentication.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint, jsonify, request, current_app
 import json
 import pyrebase
@@ -9,8 +10,32 @@ from app.helpers import is_strong_password, is_valid_email
 
 authentication = Blueprint('authentication', __name__)
 
-cred = credentials.Certificate('fbAdminConfig.json')
-pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
+config = {
+    "apiKey": os.environ.get("API_KEY"),
+    "authDomain": os.environ.get("AUTH_DOMAIN"),
+    "databaseURL": os.environ.get("DATABASE_URL"),
+    "projectId": os.environ.get("PROJECT_ID"),
+    "storageBucket": os.environ.get("STORAGE_BUCKET"),
+    "messagingSenderId": os.environ.get("MESSAGING_SENDER_ID"),
+    "appId": os.environ.get("APP_ID"),
+    "measurementId": os.environ.get("MEASUREMENT_ID")
+}
+
+# Initialize Firebase Admin using environment variables
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.environ.get("PROJECT_ID"),
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-krv15%40socialsync-35f38.iam.gserviceaccount.com"
+})
+
+pb = pyrebase.initialize_app(config)
 firebase = firebase_admin.initialize_app(cred)
 pyrebase_auth = pb.auth()
 db = pb.database()
