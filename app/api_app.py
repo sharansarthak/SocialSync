a4c21a234c9199db55ec930009de3a28d5c8e800
Sarthak <sharansarthak@gmail.com>
2023-12-05 19:48:54 -0700

    commiting removed changes

--- app/api_app.py
+++ app/api_app.py
@@ -1,11 +1,15 @@
+import datetime
 from http.client import BAD_REQUEST
 import logging
+import traceback
+from venv import logger
 from flask import Blueprint, jsonify, request, current_app
 import json
 import pyrebase
 import firebase_admin
 from firebase_admin import credentials, auth
-from requests import HTTPError
+from requests.exceptions import HTTPError
+import requests
 from werkzeug.utils import secure_filename
 from functools import wraps
 from app.helpers import is_strong_password, is_valid_email
@@ -94,15 +98,59 @@ def signup():
 
         # Save the user data in the database
         db.child("users").child(localID).set(user_data)
+        # Create user in ChatEngine
+        create_chat_engine_user(name, email, localID)
         return jsonify({'message': 'User created successfully', 'user': user}), 200
 
-    except BAD_REQUEST as bad_request_exception:
-        # Handling bad request errors
-        return jsonify({'message': 'Invalid data format: ' + str(bad_request_exception)}), 400
+    except HTTPError as http_err:
+        # Log the HTTPError object for debugging
+        print(f"HTTPError: {http_err}")
+
+        try:
+            # Parsing the string representation of HTTPError to JSON
+            error_message_str = str(http_err)
+            start = error_message_str.find('{')
+            end = error_message_str.rfind('}') + 1
+            error_json_str = error_message_str[start:end]
+            error_details = json.loads(error_json_str)
+
+            # Extracting the 'message' and 'code' from the error details
+            error_message = error_details.get('error', {}).get('message', 'An unexpected error occurred')
+            error_code = error_details.get('error', {}).get('code', 400)
+
+            # Return a response with the extracted error message
+            return jsonify({'message': error_message}), error_code
+
+        except (json.JSONDecodeError, ValueError):
+            # Fallback if parsing fails
+            return jsonify({'message': 'An unexpected error occurred'}), 400
+
     except Exception as e:
-        # Handling other exceptions
-        return jsonify({'message': 'An error occurred during signup: ' + str(e)}), 500
+        # Handle other exceptions
+        print(f"General Exception: {str(e)}")
+        return jsonify({'message': 'An error occurred during signup'}), 500
     
+def create_chat_engine_user(username, email, local_id):
+    chat_engine_private_key = os.environ.get("CHAT_ENGINE_PRIVATE_KEY") 
+    chat_user_data = {
+        "username": username,
+        "secret": local_id,  # Using Firebase localId as the secret
+        "email": email
+    }
+
+    headers = {
+        'PRIVATE-KEY': chat_engine_private_key
+    }
+
+    response = requests.post(
+        'https://api.chatengine.io/users/',
+        headers=headers,
+        json=chat_user_data
+    )
+
+    if response.status_code != 201:
+        print("Failed to create user in Chat Engine:", response.text)
+
 #Api route to get a new token for a valid user
 @api_app.route('/api/login', methods=['POST'])
 @cross_origin(supports_credentials=True)
@@ -145,6 +193,16 @@ def update_user(userID):
         print(data)
         db.child('users').child(userID).update(data)
         return jsonify({'message': 'User updated successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@api_app.route('/api/users/<userID>', methods=['GET'])
+@cross_origin(supports_credentials=True)
+@check_token
+def get_user(userID):
+    try:
+        userData = db.child('users').child(userID).get().val()
+        return jsonify(userData)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -380,6 +438,19 @@ def update_event(event_id):
     try:
         data = request.json
         event = db.child("events").child(event_id).get().val()
+        newEventData = json.dumps({
+          "event_name" : data.get('event_name'),
+          "type" : data.get('event_type'),
+          "date" : data.get('event_date'),
+          "time" : data.get('event_time'),
+          "description" : data.get('event_details'),
+          "location" : data.get('event_location'),
+          "attendees" : event.get("attendees"),
+          "price" : data.get('price'),
+          "numOfParticipants" : data.get('numOfParticipants'),
+          "target_audience" : data.get('event_target_audience'),
+          "images" : ""
+        })
         if event is not None:
             db.child("events").child(event_id).update(data)
             return jsonify(event)
@@ -403,3 +474,104 @@ def delete_event(event_id):
             return jsonify({'message': 'Event not found'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+@api_app.route('/api/picture/event/<eventID>', methods=['POST'])
+@cross_origin(supports_credentials=True)
+@check_token
+def upload_picture_event(eventID):
+    try:
+        # Check if 'picture' is present in the request files
+        if 'image' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+
+        file = request.files['image']
+        # Check if the filename is not empty
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        # Validate that the user ID is provided
+        if not eventID:
+            return jsonify({'error': 'User ID is missing'}), 400
+
+        # Check if the user exists in Firestore
+        event_doc = db.child('users').child(eventID).get()
+        print(event_doc.val(), type(event_doc))
+        if event_doc.val() is None:
+            return jsonify({'error': 'User not found'}), 404
+
+        filename = secure_filename(file.filename)
+
+        # Define the path where the file will be uploaded
+        path = "images/" + eventID
+        print(path)
+        pb_storage.child(path).put(file)
+        print(filename)
+
+        # Make the blob publicly accessible
+        url = pb_storage.child(path).get_url()
+        print(url)
+        # Update the user's document in Firestore with the picture URL
+        event_doc_dict =  event_doc.val()
+        event_doc_dict['picture_url'] = event_doc_dict['picture_url'] + url + ","
+        print(str((json.dumps(event_doc_dict))))
+        db.child('users').child(eventID).remove()
+        db.child('users').child(eventID).set(event_doc_dict)
+
+        return jsonify({'message': 'Picture uploaded successfully', 'url': url})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@api_app.route('/api/events/picture/<eventID>', methods=['GET'])
+@cross_origin(supports_credentials=True)
+@check_token
+def get_picture_event(eventID):
+    try:
+        event_doc = db.child('users').child(eventID).get()
+        if event_doc.val() is None:
+            return jsonify({'error': 'User not found'}), 404
+
+        event_data = event_doc.val()
+        picture_url = event_data.get('picture_url').split(",")
+        return jsonify({'message': 'Picture found successfully', 'url': picture_url})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@cross_origin(supports_credentials=True)
+@check_token
+@api_app.route('/api/search', methods=['GET'])
+def search():
+    try:
+        query = request.args.get('query', '').lower()
+
+        # Fetch data from Firebase
+        all_data = db.child("events").get().val()
+
+        # Convert the values of the OrderedDict into a list of dictionaries
+        event_list = list(all_data.values())
+
+        # Extract filter parameters from the request
+        event_type_filter = request.args.get('type')
+        location_filter = request.args.get('location')
+        price_min = request.args.get('price_min')
+        price_max = request.args.get('price_max')
+
+        # Apply filters to narrow down the search results
+        filtered_results = event_list
+
+        if event_type_filter:
+            filtered_results = [item for item in filtered_results if item.get('type') == event_type_filter]
+
+        if location_filter:
+            filtered_results = [item for item in filtered_results if item.get('location') == location_filter]
+
+        if price_min or price_max:
+            filtered_results = [item for item in filtered_results if price_min <= float(item.get('price', 0)) <= price_max]
+
+        # Apply text search query in "event_name" and "description" fields
+        search_results = [item for item in filtered_results if query in item.get('event_name', '').lower() or query in item.get('description', '').lower()]
+
+        return jsonify(search_results), 200
+
+    except Exception as e:
+        return jsonify({'message': 'An error occurred: ' + str(e)}), 500
